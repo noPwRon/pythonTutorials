1e409f691c454265d2780eca0a7e35f335acf19e
noPwRon <8892392+noPwRon@users.noreply.github.com>
2024-11-01 22:37:38 -0700

    added a second dataframe working with gitlens

--- data/data_prep.py
+++ data/data_prep.py
@@ -2,5 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame({"A": [1,2,3],"B":[1,2,3]})
+df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
 
+df2 = pd.DataFrame({"C": [1, 2, 3]})
